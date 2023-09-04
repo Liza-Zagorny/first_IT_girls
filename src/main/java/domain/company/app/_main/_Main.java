427b8651de6b6ef155f0edb43882534cd5af95b7
Liza Neiman <5gb.place.for.love@gmail.com>
2023-09-04 23:02:03 +0300

    second array practice finished

--- src/main/java/domain/company/app/_main/_Main.java
+++ src/main/java/domain/company/app/_main/_Main.java
@@ -1,29 +1,25 @@
 package domain.company.app._main;
 
 import domain.company.app.util.GenerateRandomInt;
-import domain.company.app.util.IntArrayPractice;
-import domain.company.app.util.StatementLoopPractise;
+import domain.company.app.util.IntArrayPractice2;
 
 import java.util.Arrays;
 
 public class _Main {
     public static void main(String[] args) {
-//        int[] arr = new int[10];
-//        int min = -50;
-//        int max = 50;
-//        GenerateRandomInt generateRandomInt = new GenerateRandomInt();
-//        IntArrayPractice intArrayPractice = new IntArrayPractice();
-//        for (int i = 0; i < 10; i++) {
-//            arr[i] = generateRandomInt.generate(min, max);
-//        }
-//        System.out.println("Оригинальный массив из 10-и рандомальных чисел между " + min + " и " + max + ":");
-//        System.out.println(Arrays.toString(arr));
-//        System.out.println("1. Количество четных чисел в массиве: " + intArrayPractice.countEvenNumbers(arr));
-//        System.out.println("2. Количество нечетных чисел в массиве: " + intArrayPractice.countOddNumbers(arr));
-//        System.out.println("3. Количество простых чисел в массиве: " + intArrayPractice.countPrimeNumbers(arr));
-//        System.out.println("4. Сумма всех чисел в массиве: " + intArrayPractice.sum(arr));
-//        System.out.println("5. Разность между суммой чисел имеющих четный индекс и суммой чисел имеющих нечетный индекс в массиве:" + intArrayPractice.diffEvenIndexOddIndex(arr));
-//        System.out.println("6. Количество нулей в массиве:" + intArrayPractice.countZeros(arr));
+        int[] arr = new int[4];
+        int min = -50;
+        int max = 50;
+        GenerateRandomInt generateRandomInt = new GenerateRandomInt();
+        IntArrayPractice2 secondPractice = new IntArrayPractice2();
+        for (int i = 0; i < 4; i++) {
+            arr[i] = generateRandomInt.generate(min, max);
+        }
+        System.out.println("Оригинальный массив из 4-x рандомальных чисел между " + min + " и " + max + ":");
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Этот же массив отсортированный от наименьшого числа к наибольшему:");
+        System.out.println(Arrays.toString(secondPractice.sortNumbers(arr)));
+
 
     }
 }
--- src/main/java/domain/company/app/util/IntArrayPractice2.java
+++ src/main/java/domain/company/app/util/IntArrayPractice2.java
@@ -1,4 +1,20 @@
 package domain.company.app.util;
 
 public class IntArrayPractice2 {
+    public int[] sortNumbers(int[] arr) {
+        int temp;
+        boolean isFinished = false;
+        do {
+            for (int i = 0; i < arr.length - 1; i++) {
+                isFinished = true;
+                if (arr[i] > arr[i + 1]) {
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    isFinished = false;
+                }
+            }
+        } while (!isFinished);
+        return arr;
+    }
 }
